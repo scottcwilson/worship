73e6766b774f7d8afb14b6abd8db9ac99505d6cd
Scott Wilson <scottcwilson@gmail.com>
2026-04-06 14:21:02 -0400

    Updates

---
+++ includes/extra_datafiles/coupon_page.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package Pages
+ * @copyright Copyright 2003-2006 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: about_us.php v1.3 $
+ */
+
+
+  define('FILENAME_COUPON_PAGE', 'Coupon');
+  define('FILENAME_DEFINE_COUPON_PAGE', 'define_coupon');
